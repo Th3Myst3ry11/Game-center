c2fb17f97eeba9d8692d5aef0dc14b16220751d7
Th3Myst3ry11 <caetanobeltor@gmail.com>
2022-05-26 13:36:54 +0100

    mudança de posição no repositorio

---
+++ cg/Robot-page/index.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const divLeft = document.querySelector('#leftSide');
+const divMiddle = document.querySelector('#Middle');
+//const divRight = document.querySelector('#rightSide');
+divMiddle.style.width = '100%';
+
+ divLeft.style.backgroundColor = '#1b1c21';
+ divLeft.style.width = '30%';
+ divLeft.style.height = 'auto';
+ 
+ //divRight.style.backgroundColor = 'blue';
+ //divRight.style.width = '100%';
+//divRight.style.height = 'auto';
+
